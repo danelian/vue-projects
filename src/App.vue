0cdf85d47506c3ed42fbd9680598277f96af5e65
danelian <141317269+danelian@users.noreply.github.com>
2024-03-30 22:47:59 +0400

    Random quote generator

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TodoList from './components/TodoList.vue'
+import RandomQuoteGenerator from './components/RandomQuoteGenerator.vue'
 </script>
 
 <template>
-  <TodoList />
+  <RandomQuoteGenerator />
 </template>
 
 <style scoped>
